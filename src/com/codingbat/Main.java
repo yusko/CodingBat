08e10040fb62f92bb63502371a6a8ffa2f031a64
Joseph Yusko <yusko@me.com>
2015-08-04 15:46:17 -0400

    method call sleepIn used

--- src/com/codingbat/Main.java
+++ src/com/codingbat/Main.java
@@ -4,5 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        WarmUp proctor = new WarmUp();
+
+        System.out.println("The parameter weekday is true if it is a weekday, and the parameter vacation is true if we are on vacation.");
+        System.out.println(" We sleep in if it is not a weekday or we're on vacation. Return true if we sleep in. ");
+
+        System.out.println("weekday and not on vacation -> " + proctor.sleepIn(false, false));
+        System.out.println("weekend and not on vacation -> " + proctor.sleepIn(true, false));
+        System.out.println("weekday and on vacation -> " + proctor.sleepIn(false, true));
+
+
     }
 }
