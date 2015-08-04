43555a917cbd818b49dcf2fcafe877b7db8d8dfb
Joseph Yusko <yusko@me.com>
2015-08-04 16:50:49 -0400

    method call added  diff21  nearhundred  missingChar  backAround

--- src/com/codingbat/Main.java
+++ src/com/codingbat/Main.java
@@ -6,13 +6,36 @@ public class Main {
 	// write your code here
         WarmUp proctor = new WarmUp();
 
-        System.out.println("The parameter weekday is true if it is a weekday, and the parameter vacation is true if we are on vacation.");
-        System.out.println(" We sleep in if it is not a weekday or we're on vacation. Return true if we sleep in. ");
+//        System.out.println("===sleepIn===");
+//        System.out.println("weekday and not on vacation -> " + proctor.sleepIn(false, false));
+//        System.out.println("weekend and not on vacation -> " + proctor.sleepIn(true, false));
+//        System.out.println("weekday and on vacation -> " + proctor.sleepIn(false, true));
+//
+//        System.out.println();
+//
+//        System.out.println("===diff21===");
+//        System.out.println("diff21(19) -> " + proctor.diff21(19));
+//        System.out.println("diff21(10) -> " + proctor.diff21(10));
+//        System.out.println("diff21(21) -> " + proctor.diff21(21));
+//
+//        System.out.println();
+//
+//        System.out.println("===nearHundred===");
+//        System.out.println("nearHundred(93) -> " + proctor.nearHundred(93));
+//        System.out.println("nearHundred(90) -> " + proctor.nearHundred(90));
+//        System.out.println("nearHundred(89) -> " + proctor.nearHundred(89));
+//        System.out.println();
 
-        System.out.println("weekday and not on vacation -> " + proctor.sleepIn(false, false));
-        System.out.println("weekend and not on vacation -> " + proctor.sleepIn(true, false));
-        System.out.println("weekday and on vacation -> " + proctor.sleepIn(false, true));
+//        System.out.println("===missingChar===");
+//        System.out.println("kitten, 1 -> " + proctor.missingChar("kitten", 1));
+//        System.out.println("kitten, 0 -> " + proctor.missingChar("kitten", 0));
+//        System.out.println("kitten, 4 -> " + proctor.missingChar("kitten", 4));
+//        System.out.println();
 
+        System.out.println("===backAround===");
+        System.out.println("cat -> " + proctor.backAround("cat"));
+        System.out.println("Hello -> " + proctor.backAround("Hello"));
+        System.out.println("a -> " + proctor.backAround("a"));
 
     }
 }
--- src/com/codingbat/WarmUp.java
+++ src/com/codingbat/WarmUp.java
@@ -9,4 +9,23 @@ public class WarmUp {
             return true;
         return false;
     }
+
+    public int diff21(int n) {
+        return (n <= 21) ? 21 - n : (n - 21) * 2;
+    }
+
+    public boolean nearHundred(int n) {
+        return Math.abs(100 - n) <= 10 || Math.abs(200 - n) <= 10 ? true : false;
+    }
+
+    public String missingChar(String str, int n) {
+        String sFront = str.substring(0, n);
+        String sBack = str.substring(n + 1, str.length());
+        return sFront + sBack;
+    }
+
+    public String backAround(String str) {
+        return str.charAt(str.length() - 1 ) + str + str.charAt(str.length() - 1 );
+    }
+
 }
